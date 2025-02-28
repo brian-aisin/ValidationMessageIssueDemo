d3d8ca04f5076f1af3cdef8bbbe5ba0fa2c9b955
Brian Futrell <brianfutrell@aisinnc.com>
2025-02-28 14:09:14 -0500

    Add project files.

---
+++ ValidationMessageIssueDemo.Shared/ToDo.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+
+namespace ValidationMessageIssueDemo.Shared
+{
+    public class ToDo
+    {
+        public string? Line { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class ToDoValidator: AbstractValidator<ToDo>
+    {
+        public ToDoValidator()
+        {
+            RuleFor(x => x.Line)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Please select a line");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Please enter a name");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(50)
+                .WithMessage("Description has a max length of 50 characters");
+        }
+    }
+}
